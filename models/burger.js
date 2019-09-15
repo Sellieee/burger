b393b392e4969dcdd3cf57be6ef169d3044e98de
Sellieee <sellieee@hotmail.com>
2019-09-15 19:40:29 +1000

    added orm functions to burger.js

--- models/burger.js
+++ models/burger.js
@@ -2,3 +2,8 @@
 var orm = require("../config/orm.js")
 
 // Calling the orm functions
+orm.selectAll("burgers");
+orm.insertOne("burgers", "burger_name", "Mushroom");
+orm.updateOne("burgers", "burger_name", "Mushroom", "Blue Grenadier");
+
+module.exports = burger;
