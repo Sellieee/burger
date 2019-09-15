148071466b35227cf10e65b6eb953368ac4ffb1c
Sellieee <sellieee@hotmail.com>
2019-09-15 14:33:39 +1000

    added seeds.sql to burgers_db

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 -- Insert data into burgers_db database, into the burgers table - burger name. 
 INSERT INTO burgers (burger_name) VALUES
- ("Wagyu"), ("Mushroom"), ("Soft Shell Crab");
+ ("Wagyu"), ("Steak"), ("Soft Shell Crab");
