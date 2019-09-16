b6e92278afef70204731f66e56c93807eed836c0
Sellieee <sellieee@hotmail.com>
2019-09-16 22:27:49 +1000

    added code to server.js

--- server.js
+++ server.js
@@ -1,2 +1,32 @@
 // Using the npm package express
 var express = require("express");
+
+var PORT = process.env.PORT || 8989;
+
+var app = express();
+
+// Serve static content from the public directory
+app.use(express.static("public"));
+
+// Middleware
+app.use(express.urlencoded({
+   extended: true
+}));
+app.use(express.json());
+
+// Use the express handlebars npm package
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+   defaultLayout: "main"
+}));
+app.set("view-engine", "handlebars");
+
+// Import routes and give the server access to them
+var routes = require("./controllers/burger_controller.js")
+
+app.use(routes);
+
+app.listen(PORT, function () {
+   console.log("Server listening on: http://localhost:" + PORT);
+});
